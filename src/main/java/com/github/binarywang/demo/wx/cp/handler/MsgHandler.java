aa8170b6bb8be4714c53ea240a4a1990262111ce
lezizijiang <lizhangzijian@126.com>
2020-10-28 17:47:31 +0800

    my application change

--- src/main/java/com/github/binarywang/demo/wx/cp/handler/MsgHandler.java
+++ src/main/java/com/github/binarywang/demo/wx/cp/handler/MsgHandler.java
@@ -3,6 +3,8 @@ package com.github.binarywang.demo.wx.cp.handler;
 import com.github.binarywang.demo.wx.cp.builder.TextBuilder;
 import com.github.binarywang.demo.wx.cp.utils.JsonUtils;
 import java.sql.*;
+
+import com.mysql.cj.x.protobuf.MysqlxDatatypes;
 import me.chanjar.weixin.common.session.WxSessionManager;
 import me.chanjar.weixin.cp.api.WxCpService;
 import me.chanjar.weixin.cp.bean.WxCpXmlMessage;
@@ -21,28 +23,22 @@ public class MsgHandler extends AbstractHandler {
     public WxCpXmlOutMessage handle(WxCpXmlMessage wxMessage, Map<String, Object> context, WxCpService cpService,
                                     WxSessionManager sessionManager) {
         final String msgType = wxMessage.getMsgType();
-        if (msgType == null) {
-            // 如果msgType没有，就自己根据具体报文内容做处理
-        }
-
-//        if (msgType.equals(WxConsts.XmlMsgType.EVENT)) {
-//
-//        }
         //TODO 组装回复消息
         String contents = wxMessage.getContent();
         String FromUserName = wxMessage.getFromUserName();
-        String driver = "com.mysql.jdbc.Driver";
+        String driver = "com.mysql.cj.jdbc.Driver";
         // 数据库连接串
         String url = "jdbc:mysql://182.92.227.90:3306/wechat";
         // 用户名
-        String username = "lzzj";
+        String username = "root";
         // 密码
         String password = "Lzzj0821.a";
         Connection conn = null;
         Statement stmt = null;
         ResultSet rs = null;
-        String content = null;
-        StringBuffer stringBuffer = new StringBuffer("您的关键词查询到的内容有\n");
+        String content;
+        StringBuffer stringBuffer = new StringBuffer();
+        String[] lst = contents.split(" ");
         try {
             // 1、加载数据库驱动（ 成功加载后，会将Driver类的实例注册到DriverManager类中）
             Class.forName(driver);
@@ -51,12 +47,42 @@ public class MsgHandler extends AbstractHandler {
             // 3、获取数据库操作对象
             stmt = conn.createStatement();
             // 4、定义操作的SQL语句
-            String sql = "select b.title from tag a, article b where a.tag =" + contents + " and b.id = a.id";
-            // 5、执行数据库操作
-            rs = stmt.executeQuery(sql);
-            // 6、获取并操作结果集
-            while(rs.next()){
-                stringBuffer.append(rs.getBigDecimal("title\n"));
+            if("#查找关键词".equals(lst[0])){
+                if(lst.length == 1){
+                    stringBuffer.append("对不起，您输入的格式有误，请按照#查找关键词 关键词1 关键词2....的格式使用，中间需要加上空格");
+                }
+                for (int i = 1; i < lst.length; i++) {
+                    String sql = "select b.title from tag a, article b where a.tag =" + lst[i] + " and b.id = a.id";
+                    // 5、执行数据库操作
+                    stringBuffer.append("您查询的关键词"+lst[i]+"对应的标题有\n");
+                    rs = stmt.executeQuery(sql);
+                    // 6、获取并操作结果集
+                    while (rs.next()) {
+                        stringBuffer.append(rs.getBigDecimal("title"));
+                        stringBuffer.append("\n");
+                    }
+                    if("".equals(stringBuffer.toString())){
+                        stringBuffer.append("您输入的这个关键词尚无记录");
+                    }
+                }
+            }else if("#订阅".equals(lst[0])){
+                if(lst.length == 1){
+                    stringBuffer.append("对不起，您输入的格式有误，请按照#订阅 关键词1 关键词2....的格式使用，中间需要加上空格");
+                }
+                for (int i = 1; i < lst.length; i++) {
+                    String sql = "insert into table subscriber values("+FromUserName+","+"lst[i]"+")";
+                    rs = stmt.executeQuery(sql);
+                }
+            }else if("#查询文章内容".equals(lst[0])){
+                if(lst.length == 1){
+                    stringBuffer.append("对不起，您输入的格式有误，请按照#查询文章内容 标题1的格式使用，中间需要加上空格");
+                }
+                for (int i = 1; i < lst.length; i++) {
+                    String sql = "select content from article where title = "+lst[i];
+                    rs = stmt.executeQuery(sql);
+                }
+            }else{
+                stringBuffer.append("您输入内容无效，请先输入");
             }
         } catch (Exception e) {
             e.printStackTrace();
@@ -90,5 +116,4 @@ public class MsgHandler extends AbstractHandler {
 
     }
     //todo 定时查看数据库
-    //todo msghandler添加订阅、查询标题
 }
